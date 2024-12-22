bc0eedc515175f59481215829dd2b4b1db79a9ef
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-22 13:21:33 +0000

    finished challenge 'Converting your ClassAPI to a functional component using Hooks.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,9 @@ import css from "./App.module.css";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
 //import Content from "./components/Content";
-import ContentHooks from "./components/ContentHooks";
+//import ContentHooks from "./components/ContentHooks";
+//import COntentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
@@ -12,7 +14,10 @@ function App() {
       <NavBarForm />
       <Sidebar />
       {/* <Content /> */}
-      <ContentHooks />
+      {/*<ContentHooks /> */}
+      {/*<ContentAPI /> */}
+      <ContentAPIHooks />
+
     </div>
   );
 }
